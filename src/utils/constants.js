2ccc93e6a18e01665cee57b6596d05e061e057d6
Jaydeep Godhani <jaydeep@Hnvk.local>
2025-05-25 18:04:18 +0530

    basepath changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,7 @@ import io from "socket.io-client";
 export const BASE_URL =
   window.location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
-  export const createSocketConnection = () => {
-    return io(BASE_URL);
-  }
+export const createSocketConnection = () => {
+  if (location.hostname === "localhost") return io(BASE_URL);
+  else return io("/", { path: "api.socket.io" });
+};
